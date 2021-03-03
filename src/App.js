ecc47dfa0a88e737f1bd1dc5c6206df9927380ca
juancfsantos <juan.santos@live.com>
2021-03-02 20:48:29 -0800

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import Title from './components/Title'
 import './App.css';
 import MovieNP from './components/MovieNowPlaying'
